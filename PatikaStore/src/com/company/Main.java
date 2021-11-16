c6efff890b5571a948cf79a2ce7dd6edac7da567
cemile-nur <78272058+cemile-nur@users.noreply.github.com>
2021-11-17 01:19:01 +0300

    products listed

--- PatikaStore/src/com/company/Main.java
+++ PatikaStore/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Brands brands = new Brands(1,new String());
-        brands.getBrandsName();
+        PatikaStore patikaStore = new PatikaStore();
+        patikaStore.start();
     }
 }
--- PatikaStore/src/com/company/Product.java
+++ PatikaStore/src/com/company/Product.java
@@ -1,11 +1,25 @@
 package com.company;
 
 public class Product {
+    private String categoryName,productName,brand;
+    private  int ram,memory,id,price,stockAmount;
+    private double discountRate,screenSize;
 
-    private String categoryName,productName,brand,color;
-    private  int ram,memory,id,price,stockAmount,screenSize,batteryPower;
-    private double discountRate;
+    public  Product(int id, String productName,String categoryName,int stockAmount,String brand,int price,double discountRate,
+                    int ram, int memory, double  screenSize ){
 
+        this.categoryName=categoryName;
+        this.productName=productName;
+        this.brand=brand;
+        this.ram=ram;
+        this.memory=memory;
+        this.id=id;
+        this.price=price;
+        this.stockAmount=stockAmount;
+        this.screenSize=screenSize;
+        this.discountRate=discountRate;
+
+    }
 
     public String getCategoryName() {
         return categoryName;
@@ -29,14 +43,6 @@ public class Product {
 
     public void setBrand(String brand) {
         this.brand = brand;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
     }
 
     public int getRam() {
@@ -79,20 +85,12 @@ public class Product {
         this.stockAmount = stockAmount;
     }
 
-    public int getScreenSize() {
+    public double  getScreenSize() {
         return screenSize;
     }
 
     public void setScreenSize(int screenSize) {
         this.screenSize = screenSize;
-    }
-
-    public int getBatteryPower() {
-        return batteryPower;
-    }
-
-    public void setBatteryPower(int batteryPower) {
-        this.batteryPower = batteryPower;
     }
 
     public double getDiscountRate() {
@@ -101,25 +99,5 @@ public class Product {
 
     public void setDiscountRate(double discountRate) {
         this.discountRate = discountRate;
-    }
-
-    public  Product(int id, String productName,String categoryName,int stockAmount,String brand,int price,double discountRate,
-                    int ram, int memory,int batteryPower,String color, int screenSize ){
-
-        this.categoryName=categoryName;
-        this.productName=productName;
-        this.brand=brand;
-        this.color=color;
-
-        this.ram=ram;
-        this.memory=memory;
-        this.id=id;
-        this.price=price;
-        this.stockAmount=stockAmount;
-        this.screenSize=screenSize;
-        this.batteryPower=batteryPower;
-
-        this.discountRate=discountRate;
-
     }
 }
