d0f24ce84e97a32844a0ead38996ad1124693746
cemile-nur <78272058+cemile-nur@users.noreply.github.com>
2021-12-02 00:42:15 +0300

    Race of Threads

---
+++ ThreadlerinYarisi/src/com/company/Timer.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Timer implements Runnable{
+
+    int index;
+
+    public Timer() {
+        this.index = 0;
+    }
+
+    @Override
+    public void run() {
+        int number = Main.numbers.get(index);
+
+        if (number % 2 == 0)
+            Main.even.add(number);
+        else
+            Main.odd.add(number);
+
+        System.out.println("İş bitti. "
+                + Thread.currentThread().getName()
+                + " Even Size:" + Main.even.size()
+                + " Odd Size: " + Main.odd.size());
+        index++;
+    }
+}
